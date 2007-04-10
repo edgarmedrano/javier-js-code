c819bc47beb86907865a8634c20364b4df7706a5
Edgar Medrano <Edgar Medrano@localhost>
2007-04-10 01:23:31 -0600

    Avance 20070409 2

--- agi/agi-test.wsh.js
+++
@@ -1,9 +0,0 @@
-function _load(libRelativePath) {
-	var fso = new ActiveXObject("Scripting.FileSystemObject");
-	var libPath = fso.BuildPath(fso.GetParentFolderName(WScript.ScriptFullName),libRelativePath);
-	var f = fso.OpenTextFile(libPath,1 /*for reading*/);
-	return f.ReadAll();
-}
-eval(_load("..\lib\stdio.wsh.js"));
-eval(_load("..\lib\agi.js"));
-eval(_load("agi-test.js"));
